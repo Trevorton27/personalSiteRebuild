4af625bf51f78513e3095618b12df822adc973c0
Trevor Mearns <spiral272@gmail.com>
2021-05-20 20:59:59 +0900

    converted Contact.js to class based component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,127 +3,138 @@ import PrimaryLayout from "../layouts/PrimaryLayout.js"
 import Header from "../components/Header"
 import { Form, Button } from "react-bootstrap"
 
-const [form, setForm] = useState({
-  name: "",
-  email: "",
-  message: "",
-})
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+    }
+    this.encode = this.encode.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-const encode = data => {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&")
-}
+  encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
 
-const handleChange = e => {
-  setForm({
-    ...form,
-    [e.target.name]: e.target.value,
-  })
-}
+  handleChange = e => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value,
+    })
+  }
 
-const handleSubmit = e => {
-  e.preventDefault()
+  handleSubmit = e => {
+    e.preventDefault()
 
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode({ "form-name": "contact", ...form }),
-  })
-    .then(() => alert("Success!"))
-    .catch(error => alert(error))
-  setForm({
-    name: "",
-    email: "",
-    message: "",
-  })
-}
-
-const IndexPage = () => (
-  <div>
-    <Header />
-    <PrimaryLayout column="col-10">
-      <div class="container contact-form" id="contact">
-        <div
-          class="contact-image"
-          style={{ textAlign: "center", marginTop: "5em", marginBottom: "3em" }}
-        >
-          <img
-            src={require("../images/trevorMearnsMailIcon.png")}
-            alt="rocket_contact"
-          />
-        </div>
-        <form
-          name="contactForm"
-          method="POST"
-          data-netlify="true"
-          style={{ marginBottom: "3em" }}
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="form-name" value="contactForm" />
-          <h2
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...form }),
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error))
+    this.setState({
+      name: "",
+      email: "",
+      message: "",
+    })
+  }
+  render() {
+    ;<div>
+      <Header />
+      <PrimaryLayout column="col-10">
+        <div class="container contact-form" id="contact">
+          <div
+            class="contact-image"
             style={{
               textAlign: "center",
-              fontSize: "50px",
-              marginBottom: "1.5em",
+              marginTop: "5em",
+              marginBottom: "3em",
             }}
           >
-            Contact Trevor
-          </h2>
-          <div class="row">
-            <div class="col-md-6">
-              <div class="form-group">
-                <input
-                  type="text"
-                  name="name"
-                  class="form-control"
-                  placeholder="Your Name *"
-                  onChange={handleChange}
-                  value={form.name}
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  name="email"
-                  class="form-control"
-                  placeholder="Your Email *"
-                  onChange={handleChange}
-                  value={form.email}
-                />
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <textarea
-                  name="message"
-                  class="form-control"
-                  placeholder="Your Message *"
-                  onChange={handleChange}
-                  value={form.message}
-                ></textarea>
-              </div>
-            </div>
-          </div>
-          <div class="form-group" style={{ textAlign: "center" }}>
-            <input
-              style={{
-                marginTop: "1.5em",
-                marginBottom: "2.5em",
-                backgroundColor: "#343a40",
-                color: "#fff",
-                padding: ".4em 1em .4em 1em",
-              }}
-              type="submit"
-              name="btnSubmit"
-              class="btnContact"
-              value="Send It"
+            <img
+              src={require("../images/trevorMearnsMailIcon.png")}
+              alt="rocket_contact"
             />
           </div>
-        </form>
-      </div>
-    </PrimaryLayout>
-  </div>
-)
+          <form
+            name="contactForm"
+            method="POST"
+            data-netlify="true"
+            style={{ marginBottom: "3em" }}
+            onSubmit={handleSubmit}
+          >
+            <input type="hidden" name="form-name" value="contactForm" />
+            <h2
+              style={{
+                textAlign: "center",
+                fontSize: "50px",
+                marginBottom: "1.5em",
+              }}
+            >
+              Contact Trevor
+            </h2>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    name="name"
+                    class="form-control"
+                    placeholder="Your Name *"
+                    onChange={handleChange}
+                    value={this.state.name}
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    name="email"
+                    class="form-control"
+                    placeholder="Your Email *"
+                    onChange={handleChange}
+                    value={this.state.email}
+                  />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <textarea
+                    name="message"
+                    class="form-control"
+                    placeholder="Your Message *"
+                    onChange={handleChange}
+                    value={this.state.message}
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+            <div class="form-group" style={{ textAlign: "center" }}>
+              <input
+                style={{
+                  marginTop: "1.5em",
+                  marginBottom: "2.5em",
+                  backgroundColor: "#343a40",
+                  color: "#fff",
+                  padding: ".4em 1em .4em 1em",
+                }}
+                type="submit"
+                name="btnSubmit"
+                class="btnContact"
+                value="Send It"
+              />
+            </div>
+          </form>
+        </div>
+      </PrimaryLayout>
+    </div>
+  }
+}
 
 export default IndexPage
