7f4f56b739da26785543c6b67bdf18673b76980c
Trevor Mearns <spiral272@gmail.com>
2021-05-20 19:02:57 +0900

    updated Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,77 +1,129 @@
-import React from "react"
+import React, { useState } from "react"
+import PrimaryLayout from "../layouts/PrimaryLayout.js"
+import Header from "../components/Header"
+import { Form, Button } from "react-bootstrap"
 
-const Contact = () => {
-  return (
-    <div class="container contact-form" id="contact">
-      <div
-        class="contact-image"
-        style={{ textAlign: "center", marginTop: "10em" }}
-      >
-        <img
-          src={require("../images/trevorMearnsMailIcon.png")}
-          alt="rocket_contact"
-        />
-      </div>
-      <form method="post" style={{ marginBottom: "3em" }}>
-        <h2
-          style={{
-            textAlign: "center",
-            fontSize: "40px",
-            marginBottom: "1.5em",
-          }}
-        >
-          Contact Trevor
-        </h2>
-        <div class="row">
-          <div class="col-md-6">
-            <div class="form-group">
-              <input
-                type="text"
-                name="txtName"
-                class="form-control"
-                placeholder="Your Name *"
-                value=""
-              />
-            </div>
-            <div class="form-group">
-              <input
-                type="text"
-                name="txtEmail"
-                class="form-control"
-                placeholder="Your Email *"
-                value=""
-              />
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="form-group">
-              <textarea
-                name="txtMsg"
-                class="form-control"
-                placeholder="Your Message *"
-                // style={width: '100%'; height: '150px';}
-              ></textarea>
-            </div>
-          </div>
-        </div>
-        <div class="form-group" style={{ textAlign: "center" }}>
-          <input
-            style={{
-              marginTop: "1.5em",
-              marginBottom: "2.5em",
-              backgroundColor: "#343a40",
-              color: "#fff",
-              padding: ".4em 1em .4em 1em",
-            }}
-            type="submit"
-            name="btnSubmit"
-            class="btnContact"
-            value="Send It"
-          />
-        </div>
-      </form>
-    </div>
-  )
+const [form, setForm] = useState({
+  name: "",
+  email: "",
+  message: "",
+})
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
 }
 
-export default Contact
+const handleChange = e => {
+  setForm({
+    ...form,
+    [e.target.name]: e.target.value,
+  })
+}
+
+const handleSubmit = e => {
+  e.preventDefault()
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encode({ "form-name": "contact", ...form }),
+  })
+    .then(() => alert("Success!"))
+    .catch(error => alert(error))
+  setForm({
+    name: "",
+    email: "",
+    message: "",
+  })
+}
+
+const IndexPage = () => (
+  <div>
+    <Header />
+    <PrimaryLayout column="col-10">
+      <div class="container contact-form" id="contact">
+        <div
+          class="contact-image"
+          style={{ textAlign: "center", marginTop: "5em", marginBottom: "3em" }}
+        >
+          <img
+            src={require("../images/trevorMearnsMailIcon.png")}
+            alt="rocket_contact"
+          />
+        </div>
+        <form
+          name="contactForm"
+          method="POST"
+          data-netlify="true"
+          style={{ marginBottom: "3em" }}
+          onSubmit={handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="contactForm" />
+          <h2
+            style={{
+              textAlign: "center",
+              fontSize: "50px",
+              marginBottom: "1.5em",
+            }}
+          >
+            Contact Trevor
+          </h2>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <input
+                  type="text"
+                  name="name"
+                  class="form-control"
+                  placeholder="Your Name *"
+                  onChange={handleChange}
+                  value={form.name}
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="text"
+                  name="email"
+                  class="form-control"
+                  placeholder="Your Email *"
+                  onChange={handleChange}
+                  value={form.email}
+                />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <textarea
+                  name="message"
+                  class="form-control"
+                  placeholder="Your Message *"
+                  onChange={handleChange}
+                  value={form.message}
+                ></textarea>
+              </div>
+            </div>
+          </div>
+          <div class="form-group" style={{ textAlign: "center" }}>
+            <input
+              style={{
+                marginTop: "1.5em",
+                marginBottom: "2.5em",
+                backgroundColor: "#343a40",
+                color: "#fff",
+                padding: ".4em 1em .4em 1em",
+              }}
+              type="submit"
+              name="btnSubmit"
+              class="btnContact"
+              value="Send It"
+            />
+          </div>
+        </form>
+      </div>
+    </PrimaryLayout>
+  </div>
+)
+
+export default IndexPage
